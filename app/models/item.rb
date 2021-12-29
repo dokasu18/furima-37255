44c03ab84c80502cd941c5661168aa63621c6c37
dokasu18 <youyume39@gmail.com>
2021-12-29 15:16:55 +0900

    出品機能単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,9 +14,8 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :comment
-    validates :price
-    validates :user_id
     validates :image
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
   
   with_options presence: true, numericality: { other_than: 1 , message: "can't be blank"} do
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                {"絵本"}
+    comment             {Faker::Lorem.sentence}
+    category_id         {2}
+    condition_id        {2}
+    cost_id             {2}
+    area_id             {2}
+    shipping_date_id    {2}
+    price               {"1000"}
+    association :user 
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
