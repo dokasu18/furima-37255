683b248dc01e66054b23025cf6e12667ddc7ad72
dokasu18 <youyume39@gmail.com>
2021-12-30 17:14:30 +0900

    商品詳細表示機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   root to: "items#index"
   
-  resources :items, only: [:new, :create, :index]
+  resources :items, only: [:new, :create, :index, :show]
 
   
 end
