f8db8355495614e0dbb5eefab0210f84118e776b
dokasu18 <youyume39@gmail.com>
2022-01-10 17:59:07 +0900

    コンフリクトの解消

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,6 +3,7 @@ class OrderAddress
   
   attr_accessor :postal_code, :area_id, :city, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
 
+  
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
